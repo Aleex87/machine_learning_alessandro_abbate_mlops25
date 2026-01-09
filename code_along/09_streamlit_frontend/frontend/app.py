3c0e7ebea2115526395348951c9120239f5c9635
Aleex87 <alessandrodanteabbate@gmail.com>
2026-01-09 11:02:32 +0100

    started streamlit on api folder 9

---
+++ code_along/09_streamlit_frontend/frontend/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.markdown("# App front page")
+
+pages = [
+    st.Page("pages/home.py", title="Home"),
+    st.Page("pages/book_finder.py", title="BookFinder"),
+    st.Page("pages/dashboard.py", title="Dashboard"),
+]
+pg = st.navigation(pages)
+
+pg.run()
